ef49cfbe8a20b11fc7a07b10851ebec584e0d94e
Mani Krishna Mandepudi <manikrishnamandepudi@Manis-MacBook-Pro.local>
2023-12-25 13:05:14 +0000

    Added code for the utils.py file

---
+++ src/mcqgenerator/utils.py
@@ -0,0 +1,44 @@
+import os
+import PyPDF2
+import json
+import traceback
+
+def read_file(file):
+    if file.name.endswith('.pdf'):
+        try:
+            pdf_reader = PyPDF2.PdfFileReader(file)
+            text = ""
+            for page in pdf_reader.pages:
+                text += page.extract_text()
+            return text
+
+        except Exception as e:
+            raise Exception("Error while reading PDF file")
+        
+    else:
+        raise Exception(
+            "Unsupported file type. Only PDF files are supported"
+            )
+    
+    def get_table_data(quiz_str):
+        try:
+            quiz_dict = json.loads(quiz_str)
+            quiz_table_data =[]
+
+            for key, value in quiz_dict.items():
+                mcq =value["mcq"]
+                options  = " || ".join(
+                    [
+                        f"(option)-> (option_value)" for option, option_value in value["options"].items()
+                    ]
+                )
+            
+                correct = value["correct"]
+                quiz_table_data.append({"MAQ": mcq,"Choices": options,"Correct": correct})
+            
+            return quiz_table_data
+        
+        except Exception as e:
+            traceback.print_exception(type(e), e, e.__traceback__)
+            return False
+        
