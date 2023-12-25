c5c1354c2e18027e5350c1f44b8d4fbae73a8dee
Mani Krishna Mandepudi <manikrishnamandepudi@Manis-MacBook-Pro.local>
2023-12-25 13:06:16 +0000

    Added code for the utils.py file

--- src/mcqgenerator/utils.py
+++ src/mcqgenerator/utils.py
@@ -20,25 +20,25 @@ def read_file(file):
             "Unsupported file type. Only PDF files are supported"
             )
     
-    def get_table_data(quiz_str):
-        try:
-            quiz_dict = json.loads(quiz_str)
-            quiz_table_data =[]
+def get_table_data(quiz_str):
+    try:
+        quiz_dict = json.loads(quiz_str)
+        quiz_table_data =[]
 
-            for key, value in quiz_dict.items():
-                mcq =value["mcq"]
-                options  = " || ".join(
-                    [
-                        f"(option)-> (option_value)" for option, option_value in value["options"].items()
-                    ]
-                )
-            
-                correct = value["correct"]
-                quiz_table_data.append({"MAQ": mcq,"Choices": options,"Correct": correct})
-            
-            return quiz_table_data
+        for key, value in quiz_dict.items():
+            mcq =value["mcq"]
+            options  = " || ".join(
+                [
+                    f"(option)-> (option_value)" for option, option_value in value["options"].items()
+                ]
+            )
         
-        except Exception as e:
-            traceback.print_exception(type(e), e, e.__traceback__)
-            return False
+            correct = value["correct"]
+            quiz_table_data.append({"MAQ": mcq,"Choices": options,"Correct": correct})
         
+        return quiz_table_data
+    
+    except Exception as e:
+        traceback.print_exception(type(e), e, e.__traceback__)
+        return False
+    
